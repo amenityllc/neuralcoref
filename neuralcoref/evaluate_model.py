ced3a8e6d0ce6ab78628dc93288242961a8b2968
stavshem <stav.shem@gmail.com>
2020-01-16 19:14:07 +0200

    wip

--- neuralcoref/evaluate_model.py
+++ neuralcoref/evaluate_model.py
@@ -1,24 +1,30 @@
 import os
 import sys
-import spacy
 from types import SimpleNamespace
 
-from neuralcoref import NeuralCoref
-from train.dataset import NCDataset
 from train.evaluator import ConllEvaluator
+from train.utils import SIZE_EMBEDDING
 
-
+from train.model import Model
+from train.dataset import (
+    NCDataset,
+    NCBatchSampler,
+    load_embeddings_from_file,
+    padder_collate,
+    SIZE_PAIR_IN,
+    SIZE_SINGLE_IN,
+)
 PACKAGE_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(PACKAGE_DIRECTORY, 'train'))
 
 
 ALL_MENTIONS_PATH = os.path.join(PACKAGE_DIRECTORY, "test_mentions.txt")
-DATA_PATH = '/Users/staveshemesh/Projects/Information-Extraction/Coref-Resolution/conll-2012/train/numpy/' #tmp
 EVAL_DATA_PATH = '/Users/staveshemesh/Projects/Information-Extraction/Coref-Resolution/conll-2012/test/' #tmp
+EVAL_DATA_NUMPY_PATH = os.path.join(EVAL_DATA_PATH, 'numpy/')
+EVAL_DATA_KEY_PATH = os.path.join(EVAL_DATA_PATH, 'key.txt')
+EMBEDDING_PATH = os.path.join(PACKAGE_DIRECTORY, 'train/weights/')
 
-nlp = spacy.load('en')
-coref = NeuralCoref(nlp.vocab)
-model = coref.model
+
 
 # Imitate learn.py's command line argument
 args = SimpleNamespace(**{
@@ -27,13 +33,25 @@ args = SimpleNamespace(**{
     "cuda": False,
     "batchsize": 20000, # Size of a batch in total number of pairs
     "numworkers": 8, # Number of workers for loading batches
+    "h1": 1000,  # Number of hidden unit on layer 1"
+    "h2": 500, # Number of hidden unit on layer 2
+    "h3": 500,  # Number of hidden unit on layer 3
 })
 
-test_key_file = f'{EVAL_DATA_PATH}/key.txt'
-dataset = NCDataset(DATA_PATH, args)
-embed_path = None
-test_data_path = f'{EVAL_DATA_PATH}/numpy/'
+dataset = NCDataset(EVAL_DATA_NUMPY_PATH, args)
+tensor_embeddings, voc = load_embeddings_from_file(EMBEDDING_PATH + "tuned_word")
 
-evaluator = ConllEvaluator(model, dataset, test_data_path, test_key_file, embed_path, args)
-evaluator.build_test_file(out_path=ALL_MENTIONS_PATH, print_all_mentions=True)
+model = Model(
+    len(voc),
+    SIZE_EMBEDDING,
+    args.h1,
+    args.h2,
+    args.h3,
+    SIZE_PAIR_IN,
+    SIZE_SINGLE_IN,
+)
+model.load_embeddings(tensor_embeddings)
+
+evaluator = ConllEvaluator(model, dataset, EVAL_DATA_NUMPY_PATH, EVAL_DATA_KEY_PATH, None, args)
+evaluator.build_test_file(out_path=ALL_MENTIONS_PATH, remove_singleton=False, print_all_mentions=False)
 evaluator.get_score(ALL_MENTIONS_PATH, debug=True)
